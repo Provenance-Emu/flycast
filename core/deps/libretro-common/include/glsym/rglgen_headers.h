bfbccce1f2bc8861ac6ef5629a5decfd9abb5065
Joseph Mattiello <git@joemattiello.com>
2025-01-06 12:31:35 -0500

    force es3 includes, add version.h
    Signed-off-by: Joseph Mattiello <git@joemattiello.com>

--- core/deps/libretro-common/include/glsym/rglgen_headers.h
+++ core/deps/libretro-common/include/glsym/rglgen_headers.h
@@ -41,8 +41,8 @@
 #elif defined(__APPLE__)
 #include <compat/apple_compat.h>
 #if MAC_OS_X_VERSION_10_7
-#include <OpenGLES/ES2/gl.h>
-#include <OpenGLES/ES2/glext.h>
+#include <OpenGLES/ES3/gl.h>
+#include <OpenGLES/ES3/glext.h>
 #else
 #include <OpenGL/gl.h>
 #include <OpenGL/glext.h>
---
+++ core/version.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define GIT_VERSION		"v2.4-33-gb91f82c7e"
+#define GIT_HASH		"b91f82c7e"
+#define BUILD_DATE		"2024-12-06T23:32:52Z"
